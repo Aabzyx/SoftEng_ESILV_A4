9c69c132720f5782f314dcef881dcb4b1ee8b000
mustache956 <antoine.rincheval@gmail.com>
2022-06-21 12:20:59 +0200

    front homePage

--- code/Frontend/src/components/HomePage.vue
+++ code/Frontend/src/components/HomePage.vue
@@ -1,8 +1,40 @@
 <template>
   <p>HomePage</p>
+  <div>
+    
+    <div v-for="(election, index) in (elections)" :key="index">
+      <p> public votes :</p>
+      <div v-if="election.type === 'informerlle' ">
+        <p>{{election.name}}</p>
+        <img :src=election.image>
+        <div v-on:click="deleteElection(vote)">
+          <a>❌</a>
+        </div>
+        <div v-on:click="test()"><!--reflechir a comment rediriger!-->
+          <a>Vote</a>
+        </div>
+      </div>
+    </div>
+    
+    <div v-for="(election, index) in (elections)" :key="index">
+      <p> official votes :</p>
+      <div v-if="election.type === 'officiel' ">
+        <p>{{election.name}}</p>
+        <img :src=election.image>
+        <div class="btn btn-primary" v-on:click="test()"><!--reflechir a comment rediriger!-->
+          <a>Vote</a>
+        </div>
+      </div>
+    </div>
+    <div>
+      <router-link class="btn btn-primary" to="/createVote">create vote</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: "HomePage",
   data() {
@@ -10,7 +42,7 @@ export default {
     };
   },
   computed: {
-
+    ...mapActions(['votes']),
   },
   methods:{
 
