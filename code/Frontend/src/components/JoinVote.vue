8ec6a17ed02bbec98fc83f6deafcca1fdd29d084
romainpasquier <romain.pasquier@efrei.net>
2022-06-22 23:38:59 +0200

    front page luka

--- code/Frontend/src/components/JoinVote.vue
+++ code/Frontend/src/components/JoinVote.vue
@@ -1,4 +1,5 @@
 <template>
+  <br><br>
   <div class="login-root">
     <div class="box-root flex-flex flex-direction--column" style="min-height: 100vh;flex-grow: 1;">
       <div class="loginbackground box-background--white padding-top--64">
