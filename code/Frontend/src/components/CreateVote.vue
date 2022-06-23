a29374fd202ad2df3a724d76fca454e583545bad
mustache956 <antoine.rincheval@gmail.com>
2022-06-23 14:06:06 +0200

    Update CreateVote.vue
    Form pour createVote bien basique

--- code/Frontend/src/components/CreateVote.vue
+++ code/Frontend/src/components/CreateVote.vue
@@ -1,5 +1,16 @@
 <template>
   <p>CreateVote</p>
+  <div>
+    <label><b>nom du vote :</b></label>
+    <input name="nomVote" placeholder="nom du vote" v-model="text">
+    <label><b>description :</b></label>
+    <input name="description" placeholder="description" v-model="textarea">
+    <label><b>choix logo :</b></label>
+    <input name="logoVote" placeholder="Entrez l'URL" v-model="text">
+    <label><b>nombre de votants :</b></label>
+    <input name="nombreVotants" type="number">
+    <input type="submit" value="next" v-on:click="continueVote()">
+  </div>
 </template>
 
 <script>
