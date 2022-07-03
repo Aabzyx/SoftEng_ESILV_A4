8836f2a321995305d81c0fcd303991a0ecd999b3
romainpasquier <romain.pasquier@efrei.net>
2022-07-03 03:45:26 +0200

    enlever des routeus show results

--- code/Frontend/src/components/Header.vue
+++ code/Frontend/src/components/Header.vue
@@ -1,6 +1,5 @@
 <template>
     <nav class="container-fluid">
-      <router-link class="mr-1 btn btn-primary" to="/ShowResultatsVue" >ShowResultatsVue</router-link>
       <router-link class="mr-1 btn btn-primary" to="/UpdatePassword">PasswordUpadte</router-link>
       <router-link class="mr-1 btn btn-primary" to="/TestImage">TestImage</router-link>
     </nav>
