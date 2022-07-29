cd2a524a92c3148f5d75a5540ccb5ace10ba89a8
Romain Pasquier <romain.pasquier@efrei.net>
2022-07-29 22:40:00 +0200

    deploiement

--- template-code/Frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from '@/store' // short for @/store/index
-import router from './router'
-
-createApp(App).use(router).use(store).mount('#app')
